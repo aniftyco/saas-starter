53b0aad6bba2025482d5bce0e54ea5c096f79a76
Josh Manders <josh@joshmanders.com>
2023-09-13 23:17:25 -0500

    add unit tests and fix functional test

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -4,5 +4,5 @@ test('display welcome page', async ({ client }) => {
   const response = await client.get('/');
 
   response.assertStatus(200);
-  response.assertTextIncludes('<h1 class="title"> It Works! </h1>');
+  response.assertTextIncludes('It Works!');
 });
---
+++ tests/unit/example.spec.ts
@@ -0,0 +1,5 @@
+import { test } from '@japa/runner';
+
+test('It should work', ({ assert }) => {
+  assert.equal(1, 1);
+});
