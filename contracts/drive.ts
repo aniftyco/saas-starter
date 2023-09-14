1c298292090cf089a903e5a1b00d5cc03cb73923
Josh Manders <josh@joshmanders.com>
2023-09-13 23:09:49 -0500

    update import paths

--- contracts/drive.ts
+++ contracts/drive.ts
@@ -6,7 +6,7 @@
  */
 
 import type { InferDisksFromConfig } from '@adonisjs/core/build/config';
-import type driveConfig from '../config/drive';
+import type driveConfig from '@config/drive';
 
 declare module '@ioc:Adonis/Core/Drive' {
   interface DisksList extends InferDisksFromConfig<typeof driveConfig> {}
--- contracts/hash.ts
+++ contracts/hash.ts
@@ -6,7 +6,7 @@
  */
 
 import type { InferListFromConfig } from '@adonisjs/core/build/config';
-import type hashConfig from '../config/hash';
+import type hashConfig from '@config/hash';
 
 declare module '@ioc:Adonis/Core/Hash' {
   interface HashersList extends InferListFromConfig<typeof hashConfig> {}
