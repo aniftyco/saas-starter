41d14a9e688918085adbec1e4d51d0dc416ac54f
Josh Manders <josh@joshmanders.com>
2023-09-26 14:39:22 -0500

    allow method spoofing

--- config/app.ts
+++ config/app.ts
@@ -52,7 +52,7 @@ export const http: ServerConfig = {
   | so on.
   |
   */
-  allowMethodSpoofing: false,
+  allowMethodSpoofing: true,
 
   /*
   |--------------------------------------------------------------------------
