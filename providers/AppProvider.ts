6b4e8bc36680e0ccef4afb6eff28bbe6c65dab02
Josh Manders <josh@joshmanders.com>
2023-10-02 15:51:56 -0500

    add tabler icons and clsx/tailwind merge support to views

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -1,3 +1,9 @@
+import clsx from 'clsx';
+import { addCollection, edgeIconify } from 'edge-iconify';
+import { twMerge } from 'tailwind-merge';
+
+import { icons as tablerIcons } from '@iconify-json/tabler';
+
 import type { ApplicationContract } from '@ioc:Adonis/Core/Application';
 
 export default class AppProvider {
@@ -13,6 +19,14 @@ export default class AppProvider {
 
   public async ready() {
     // App is ready
+    const View = this.app.container.use('Adonis/Core/View');
+
+    addCollection(tablerIcons);
+    View.use(edgeIconify);
+
+    View.global('clsx', (...args: any[]) => {
+      return twMerge(clsx(...args));
+    });
   }
 
   public async shutdown() {
