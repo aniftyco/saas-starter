d0ff561c7dc51018879d5ba046f2c5fba7a9c5f8
Josh Manders <josh@joshmanders.com>
2023-10-13 12:43:50 -0500

    delete with session model so we utilize soft deletes correctly

--- app/Controllers/Auth/SignOutController.ts
+++ app/Controllers/Auth/SignOutController.ts
@@ -1,9 +1,12 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { DateTime } from 'luxon';
+import Session from '@app/Models/Session';
 
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 export default class SignOutController {
   public async handle({ auth, response, session }: HttpContextContract) {
-    await auth.user!.related('sessions').query().where('id', session.sessionId).update({ deletedAt: DateTime.now() });
+    const activeSession = await Session.find(session.sessionId);
+
+    await activeSession!.delete();
+
     await auth.logout();
 
     return response.redirect().toRoute('sign-in');
