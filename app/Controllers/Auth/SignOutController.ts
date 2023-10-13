5d99907243756b63b074a08b72190b237c9f7b99
Josh Manders <josh@joshmanders.com>
2023-10-12 21:51:22 -0500

    delete current session record on sign out

--- app/Controllers/Auth/SignOutController.ts
+++ app/Controllers/Auth/SignOutController.ts
@@ -1,7 +1,9 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import { DateTime } from 'luxon';
 
 export default class SignOutController {
-  public async handle({ auth, response }: HttpContextContract) {
+  public async handle({ auth, response, session }: HttpContextContract) {
+    await auth.user!.related('sessions').query().where('id', session.sessionId).update({ deletedAt: DateTime.now() });
     await auth.logout();
 
     return response.redirect().toRoute('sign-in');
