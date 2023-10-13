1efa5901d08c64b3a38f3498b3fff9fedc3fe6bc
Josh Manders <josh@joshmanders.com>
2023-10-13 12:41:27 -0500

    add listing active sessions

--- app/Controllers/DashboardController.ts
+++ app/Controllers/DashboardController.ts
@@ -1,7 +1,22 @@
+import uaParser from 'ua-parser-js';
+
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 
 export default class DashboardController {
-  public async handle({ view }: HttpContextContract) {
-    return view.render('pages/dashboard');
+  public async handle({ auth, view, session }: HttpContextContract) {
+    const sessions = (await auth.user!.related('sessions').query().whereNull('deletedAt'))
+      .sort((a, b) => {
+        if (a.id === session.sessionId) return -1;
+        if (b.id === session.sessionId) return 1;
+        return (b.createdAt.toJSDate() as any) - (a.createdAt.toJSDate() as any);
+      })
+      .map((session) => ({
+        id: session.id,
+        agent: uaParser(session.agent),
+        ip: session.ip,
+        creaedAt: session.createdAt,
+      }));
+
+    return view.render('pages/dashboard', { sessionId: session.sessionId, sessions });
   }
 }
