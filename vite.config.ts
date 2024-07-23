517da0c9fbdb91e0bbee46a3363cc5ec46dba6af
Josh Manders <josh@joshmanders.com>
2024-07-23 14:39:31 -0500

    move postcss config directly into vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import vue from '@vitejs/plugin-vue';
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [
     tsconfigPaths(),
     vue(),
