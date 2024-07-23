db5bf30c2f19c7b9aad8e513e88bcad8806d131c
Josh Manders <josh@joshmanders.com>
2024-07-22 22:21:39 -0500

    fix tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from 'tailwindcss';
+import colors from 'tailwindcss/colors';
 
 export default {
   content: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php', './resources/client/**/*.vue'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.indigo,
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
