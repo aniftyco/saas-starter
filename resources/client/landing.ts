a0639abf0f680083d6d11d2c83882171c7140a74
Josh Manders <josh@joshmanders.com>
2025-07-24 18:23:46 -0500

    configure dark/light mode

--- resources/client/landing.ts
+++ resources/client/landing.ts
@@ -7,3 +7,14 @@ window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 Livewire.start();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  document.documentElement.setAttribute('data-theme', prefersDark ? 'dark' : 'light');
+
+  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (event) => {
+    document.documentElement.setAttribute('data-theme', event.matches ? 'dark' : 'light');
+  });
+
+  document.body.removeAttribute('x-cloak');
+});
